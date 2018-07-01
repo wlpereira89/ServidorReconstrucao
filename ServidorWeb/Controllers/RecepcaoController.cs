f9cbe153df54fc65ac7ce6a05c7aa1338c9cb81d
Wagner L. Pereira <wagnerluis1989@gmail.com>
2018-07-01 04:50:58 -0300

    Criação de metodos rest para recepção e descompactação de arquivos rar de até 4000 kb por parte

--- ServidorWeb/Controllers/RecepcaoController.cs
+++ ServidorWeb/Controllers/RecepcaoController.cs
@@ -4,8 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using NUnrar.Archive;
+using NUnrar.Common;
 using ServidorWeb.Models;
 
 namespace ServidorWeb.Controllers
@@ -13,9 +16,25 @@ namespace ServidorWeb.Controllers
     [RoutePrefix("api/recepcao")]
     public class RecepcaoController : ApiController
     {
+        [AcceptVerbs("GET")]
+        [Route("arquivo/{nome}")]
+        public string UnRar(string nome)
+        {
+            string root = System.Web.HttpContext.Current.Server.MapPath("~/Data/" + nome + "/"); //.Replace(':', ' ') + DateTime.Now.ToShortDateString().Replace('/', '-')
+            try
+            {
+                RarArchive.WriteToDirectory(root + "Imagem-A.part001.rar", root, ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+            }
+            catch(FileNotFoundException ex)
+            {
+                
+            }
+            return nome + " Recebimento feito com sucesso";
+        }
+        
         [AcceptVerbs("POST")]
-        [Route("arquivo")]
-        public Task<HttpResponseMessage> ReceberArquivo()
+        [Route("arquivo/{nome}")]
+        public async Task<HttpResponseMessage> ReceberArquivoAsync(string nome)
         {
             
             HttpRequestMessage request = Request;
@@ -24,16 +43,20 @@ namespace ServidorWeb.Controllers
             {
                 throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
             }
-            
-            string root = System.Web.HttpContext.Current.Server.MapPath("~/Data/"+DateTime.Now.ToString().Replace('/', '-').Replace(':', ' '));
+
+            string root = System.Web.HttpContext.Current.Server.MapPath("~/Data/" + nome +"/"); //.Replace(':', ' ') + DateTime.Now.ToShortDateString().Replace('/', '-')
             if (!Directory.Exists(root))
             {
                 Directory.CreateDirectory(root);
             }
-            MultipartFormDataStreamProvider provider = new CustomMultipartFormDataStreamProvider(root);
+            MultipartFormDataStreamProvider provider = new CustomMultipartFormDataStreamProvider(root); 
             var task = request.Content.ReadAsMultipartAsync(provider);
-            return task.ContinueWith(o =>
+            return await task.ContinueWith(o =>
                 {
+                    if (o.IsFaulted || o.IsCanceled)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, o.Exception);
+                    } 
                     return new HttpResponseMessage()
                     {
                         Content = new StringContent("Arquivo recebido com sucesso!")
